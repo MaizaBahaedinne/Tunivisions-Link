ceb70a80f6823b834c97b2a75a7b465a4e734377
Maiza Bahaedinne <bahaedinne.maiza@esprit.tn>
2019-10-26 18:22:51 +0100

    Update Club_model.php

--- application/models/Club_model.php
+++ application/models/Club_model.php
@@ -77,10 +77,11 @@ class Club_model extends CI_Model
     {
         $this->db->select('BaseTbl.clubID , Users.userId , Users.name as Tname , Users.roleId , Users.cellule , Users.avatar , Roles.role');
         $this->db->from('tbl_club as BaseTbl');
-        $this->db->join('tbl_users as Users', 'Users.ClubID = BaseTbl.clubID', 'LEFT');
         $this->db->join('tbl_roles as Roles', 'Users.roleId = Roles.roleId', 'LEFT');
+        $this->db->join('tbl_users as Users', 'Users.ClubID = BaseTbl.clubID', 'LEFT');
+        
        
-        $this->db->where('Users.roleId=1 or Users.roleId=2 or Users.roleId=3 ') ;
+        $this->db->where('Users.roleId=1 or Users.roleId=2 or Users.roleId=3 , Users.roleId=6  ') ;
        $this->db->where('BaseTbl.clubID=',$clubID) ;
         $this->db->order_by('Users.roleId', 'ASC') ;
         
