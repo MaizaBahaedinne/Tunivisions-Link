a6bdf9b623f3de1357a4b4f93ef0d5f644fe4483
Maiza Bahaedinne <bahaedinne.maiza@esprit.tn>
2019-10-27 21:00:41 +0100

    Update User.php

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -422,10 +422,10 @@ class User extends BaseController
                 $config = array(
                     'protocol' => 'smtp', // 'mail', 'sendmail', or 'smtp'
                     'smtp_host' => 'tunivisions.link',
-                    'smtp_port' => 25,
+                    'smtp_port' => 465,
                     'smtp_user' => 'no-reply',
                     'smtp_pass' => 'Tunivisions-Link-2019',
-                    'smtp_crypto' => 'ssl', //can be 'ssl' or 'tls' for example
+                    'smtp_crypto' => 'tls', //can be 'ssl' or 'tls' for example
                     'mailtype' => 'html', //plaintext 'text' mails or 'html'
                     'smtp_timeout' => '4', //in seconds
                     'charset' => 'iso-8859-1',
