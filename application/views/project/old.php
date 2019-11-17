6af24ca9d111c515dc6183bb79c5e5f134a60bc0
Maiza Bahaedinne <bahaedinne.maiza@esprit.tn>
2019-11-17 19:50:11 +0100

    Update old.php

--- application/views/project/old.php
+++ application/views/project/old.php
@@ -98,7 +98,7 @@
               foreach($projectRecords as $record)
             {
             ?>
-    <div class="col-md-12">
+    <div class="col-md-10">
         <!--begin:: Portlet-->
         <div class="kt-portlet kt-portlet--height-fluid">
         
@@ -170,6 +170,9 @@
                         }
                     }
                     ?>
+    <div class="col-md-2" style="background-color: red">
+            
+    </div>                
 </div>
 <!--End::Section-->
 
