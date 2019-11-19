4de521318df5f4c8833ef3a38c8146ba0a30bc9c
Maiza Bahaedinne <bahaedinne.maiza@esprit.tn>
2019-11-19 21:34:37 +0100

    Update dashboard.php

--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -182,7 +182,12 @@
 
   <div class="row"> 
     <div class="col-md-6"> 
-            <iframe src="https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2FTunivisionsfoundation%2Fvideos%2F572029873566635%2F&width=auto&show_text=true&height=559&appId" width="auto" height="auto" style="border:none;overflow:auto" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media" allowFullScreen="true"></iframe>
+    <div id="fb-root"></div>
+    <script async defer crossorigin="anonymous" src="https://connect.facebook.net/fr_FR/sdk.js#xfbml=1&version=v5.0"></script>
+    <div class="fb-video" data-href="https://www.facebook.com/Tunivisionsfoundation/videos/572029873566635/" data-width="auto" data-show-text="false"><blockquote cite="https://developers.facebook.com/Tunivisionsfoundation/videos/572029873566635/" class="fb-xfbml-parse-ignore"><a href="https://developers.facebook.com/Tunivisionsfoundation/videos/572029873566635/">وقتاش TFM4.0</a><p>وقتاش TFM4.0 
+    #TunivisionsFoundation 
+    #NowOrNever 
+    #Staytuned</p>Publiée par <a href="https://www.facebook.com/Tunivisionsfoundation/">Tunivisions Foundation</a> sur Mardi 19 novembre 2019</blockquote></div>
          </div>
     
     <div class="col-md-6">     
