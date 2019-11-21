2ad66e2a69bc1c1fb6bb962737ea16f7622ec9b2
Maiza Bahaedinne <bahaedinne.maiza@esprit.tn>
2019-11-21 19:14:11 +0100

    Update TFM.php

--- application/controllers/TFM.php
+++ application/controllers/TFM.php
@@ -11,16 +11,16 @@ class TFM extends BaseController {
         parent::__construct();
         $this->load->model('user_model');
         $this->load->model('notification_model');
-        $this->load->model('Tfm_model');
+        $this->load->model('tfm_model');
         $this->isLoggedIn();   
     }
     
 
 		public function TFMListing()
 		        {
-		        	
+
 		                $searchText='' ;
-		                $data['tfmRecords'] = $this->Tfm_model->TFMListing();
+		                $data['tfmRecords'] = $this->tfm_model->TFMListing();
 		                $this->global['pageTitle'] = 'CodeInsect : club  Listing';
 		             	$this->global['active'] = 'TFM';
 		                $this->loadViews("TFM/list", $this->global, $data, NULL);   
@@ -30,7 +30,7 @@ class TFM extends BaseController {
 		public function partant($tfmId)
 		        {
 		                $searchText='' ;
-		                $data['tfmRecords'] = $this->Tfm_model->TFMListing();
+		                $data['tfmRecords'] = $this->tfm_model->TFMListing();
 		                $this->global['pageTitle'] = 'CodeInsect : club  Listing';
 		             	$this->global['active'] = 'TFM';
 		                $this->loadViews("TFM/partant", $this->global, $data, NULL);   
