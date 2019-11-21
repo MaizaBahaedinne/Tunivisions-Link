1254399ebdbf06014a4e6ce5a4c900fbfac54b85
Maiza Bahaedinne <bahaedinne.maiza@esprit.tn>
2019-11-21 19:16:18 +0100

    Update header.php

--- application/views/includes/header.php
+++ application/views/includes/header.php
@@ -235,6 +235,18 @@
 				</a>
 			</li>
 			<?php } ?>
+			</li>
+			<?php if ($SA == 1 ) { ?>
+			<li class="kt-menu__item  kt-menu__item<?php if( $active == "Evaluation" ){echo("--active") ;} ?> " aria-haspopup="true" >
+				<a  href="<?php echo base_url() ; ?>TFM/TFMListing" class="kt-menu__link ">
+					<i class="kt-menu__link-icon flaticon-exclamation">
+					</i>
+					<span class="kt-menu__link-text">
+						TFM 
+					</span>
+				</a>
+			</li>
+			<?php } ?>
 
 
 			<li class="kt-menu__section ">
