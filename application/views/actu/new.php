42790ab9fcddb3e9bd658920619a5759623d6201
Maiza Bahaedinne <bahaedinne.maiza@esprit.tn>
2019-11-21 19:17:42 +0100

    Update new.php

--- application/views/actu/new.php
+++ application/views/actu/new.php
@@ -10,7 +10,7 @@
 
 <div class="kt-content  kt-grid__item kt-grid__item--fluid kt-grid kt-grid--hor" id="kt_content">
                                             <!-- begin:: Content Head -->
-
+ 
 <!-- end:: Content Head -->                 
                     <!-- begin:: Content -->
     <div class="kt-container  kt-container--fluid  kt-grid__item kt-grid__item--fluid">
