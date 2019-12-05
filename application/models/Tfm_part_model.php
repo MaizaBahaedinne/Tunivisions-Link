fabbe2d2f398347de6721ab56e6f5373e7775828
Maiza Bahaedinne <bahaedinne.maiza@esprit.tn>
2019-12-05 20:10:36 +0100

    Update Tfm_part_model.php

--- application/models/Tfm_part_model.php
+++ application/models/Tfm_part_model.php
@@ -47,7 +47,7 @@ class Tfm_part_model extends CI_Model
         $this->db->join('tbl_club as Clubs', 'Clubs.clubID = Users.ClubID', 'LEFT');
         $this->db->join('tbl_roles as Role', 'Role.roleId = Users.roleId','left');
         $this->db->join('tbl_users as Usersr1', 'Usersr1.userId = BaseTbl.recepteurTranche1', 'LEFT');
-        $this->db->where('BaseTbl.tfmId = 6 or BaseTbl.statut = 3 ' );
+        $this->db->where('BaseTbl.tfmId = 6 and (BaseTbl.statut = 3 or BaseTbl.statut = 1 )  ' );
 
         $this->db->where('Users.clubID =', $clubId);    
         $this->db->order_by('BaseTbl.dateInscrip','DESC');  
@@ -73,7 +73,7 @@ class Tfm_part_model extends CI_Model
         
         $this->db->where('BaseTbl.tfmId =',6);
         $this->db->where('BaseTbl.p_tranch1 =',80);
-        
+
         $this->db->group_by('Users.clubID ');
         $this->db->order_by('BaseTbl.dateInscrip','DESC');  
         $query = $this->db->get();
