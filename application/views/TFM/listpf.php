be40903755729f723fd2c9af5e70db6677a32157
Maiza Bahaedinne <bahaedinne.maiza@esprit.tn>
2019-12-05 20:59:06 +0100

    Update listpf.php

--- application/views/TFM/listpf.php
+++ application/views/TFM/listpf.php
@@ -183,7 +183,7 @@
                  
                       
                         <?php if ($record->p_tranch2 > 0 ) { ?> 
-                        <span>Tranche 2 :</span>  <span class="kt-badge kt-badge--primary kt-badge--dot"></span>&nbsp;<span class="kt-font-bold kt-font-primary">Validé</span><br><?php } ?>
+                        <span>Tranche 1 :</span>  <span class="kt-badge kt-badge--primary kt-badge--dot"></span>&nbsp;<span class="kt-font-bold kt-font-primary">Validé <small>par : <?php echo $record->recp2 ; ?> le  <?php echo $record->dateTranche2; ?> </small></span><br><?php } ?>
 
 
               
@@ -193,13 +193,17 @@
                    
 
                     <td> 
-                          <?php } if ($record->statut == 1 ) { ?> 
+                          <?php  if ($record->statut == 1 ) { ?> 
                             <span class="kt-badge kt-badge--success kt-badge--dot"></span>&nbsp;<span class="kt-font-bold kt-font-success">validé</span><br> 
                           <?php } ?>
+
                           <?php if ($record->statut == 2 ) { ?> 
                          <span class="kt-badge kt-badge--danger kt-badge--dot"></span>&nbsp;<span class="kt-font-bold kt-font-danger">Non validé</span><br>
+                         <?php } ?>
+
                          <?php } if ($record->statut == 3 ) { ?> 
-                           <span class="kt-badge kt-badge--warning kt-badge--dot"></span>&nbsp;<span class="kt-font-bold kt-font-warning">pre-validé</span><br> 
+                           <span class="kt-badge kt-badge--warning kt-badge--dot"></span>&nbsp;<span class="kt-font-bold kt-font-warning">pre-validé</span><br>
+                            <?php } ?> 
 
                     </tr>
                     <?php
