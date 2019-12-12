2916a326a11cc84cbfeead33a249b9e8dbb8fdc9
Maiza Bahaedinne <bahaedinne.maiza@esprit.tn>
2019-12-12 01:20:20 +0100

    Update codeABare.php

--- application/views/TFM/codeABare.php
+++ application/views/TFM/codeABare.php
@@ -48,14 +48,8 @@ header("Access-Control-Allow-Methods: GET, OPTIONS");
                         echo "<p class='inline'><span ><b>Nom:".$product."</b> <br> Club : ".$record->ClubName."  </span>"
                         ?>
                         <div id="barcode<?php echo $id ?>"><?php echo $id ; ?></div>
-                         <?php echo "<span ><b>TFM ID: ".$rate." </b><span></p>&nbsp&nbsp&nbsp&nbsp";
-                    
-
-                            ?>
-                        <?php echo "<span ><b>Tunimateur ID: ".$rate." </b><span></p>&nbsp&nbsp&nbsp&nbsp";
-                    
-
-                            ?>
+                         <?php echo "<span ><b>TFM ID: ".$record->id." </b><span></p><span ><b>Tunimateur ID: ".$rate." </b><span></p>&nbsp&nbsp&nbsp&nbsp";                         ?>
+         
                             <script type="text/javascript">
 
                           function get_object(id) {
@@ -70,7 +64,7 @@ header("Access-Control-Allow-Methods: GET, OPTIONS");
                                        return object;
                                       }
 
-                                    get_object("barcode").innerHTML=ConnectCode_Encode_Code128A(get_object("barcode<?php echo $id ?>").innerHTML);
+                                    get_object("barcode<?php echo $id ?>").innerHTML=ConnectCode_Encode_Code128A(get_object("barcode<?php echo $id ?>").innerHTML);
                                     get_object("barcode_text").innerHTML=Get_HRText();
 
                         </script>
