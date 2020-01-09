bea6db627dcbc1a30dde624e447c272493de07c7
Maiza Bahaedinne <bahaedinne.maiza@esprit.tn>
2020-01-09 23:31:37 +0100

    Update header.php

--- application/views/includes/header.php
+++ application/views/includes/header.php
@@ -869,6 +869,7 @@
                 </div>
               </div>
             </li>
+            <!--
 						<li class="nav-item dropdown nav-messages">
 							<a class="nav-link dropdown-toggle" href="#" id="messageDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
 								<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-mail"><path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"></path><polyline points="22,6 12,13 2,6"></polyline></svg>
@@ -945,6 +946,7 @@
 								</div>
 							</div>
 						</li>
+            -->
 						<li class="nav-item dropdown nav-notifications">
 							<a class="nav-link dropdown-toggle" href="#" id="notificationDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
 								<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-bell"><path d="M18 8A6 6 0 0 0 6 8c0 7-3 9-3 9h18s-3-2-3-9"></path><path d="M13.73 21a2 2 0 0 1-3.46 0"></path></svg>
@@ -995,12 +997,12 @@
 						</li>
 						<li class="nav-item dropdown nav-profile">
 							<a class="nav-link dropdown-toggle" href="#" id="profileDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-								<img src="<?php echo base_url() ?>Uploads/avatar/<?php echo $avatar ?>" alt="profile">
+								<img src="<?php echo base_url() ?>uploads/avatar/<?php echo $avatar ?>" alt="profile">
 							</a>
 							<div class="dropdown-menu" aria-labelledby="profileDropdown">
 								<div class="dropdown-header d-flex flex-column align-items-center">
 									<div class="figure mb-3">
-										<img src="<?php echo base_url() ?>Uploads/avatar/<?php echo $avatar ?>" alt="">
+										<img src="<?php echo base_url() ?>uploads/avatar/<?php echo $avatar ?>" alt="">
 									</div>
 									<div class="info text-center">
 										<p class="name font-weight-bold mb-0"><?php echo $name ?></p>
