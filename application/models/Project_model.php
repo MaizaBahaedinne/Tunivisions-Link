63f47333c0299a41cbb5298afbf32f7cd39a2f04
Maiza Bahaedinne <bahaedinne.maiza@esprit.tn>
2020-01-13 14:57:10 +0100

    Update Project_model.php

--- application/models/Project_model.php
+++ application/models/Project_model.php
@@ -21,8 +21,10 @@ class Project_model extends CI_Model
         if($serchText !=''){
         $this->db->where('BaseTbl.titre Like ','%'.$serchText.'%'); 
         }
-        $this->db->where('Evaluations.statut =','fini AND Evaluations.validBy = 0 ') ;
+
         $this->db->where('NOW() > BaseTbl.endDate ') ;
+        $this->db->where('Evaluations.statut =','fini') ;
+        $this->db->where('Evaluations.validBy = 0 ') ;
         $this->db->order_by('BaseTbl.startDate','ASC');
 
         $query = $this->db->get();
