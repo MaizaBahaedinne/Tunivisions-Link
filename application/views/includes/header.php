f7a0ad9d99c9d29bb112340df81a916276c0652b
Maiza Bahaedinne <bahaedinne.maiza@esprit.tn>
2020-01-15 16:01:05 +0100

    Update header.php

--- application/views/includes/header.php
+++ application/views/includes/header.php
@@ -610,7 +610,7 @@
   }
 }</style><style type="text/css">@keyframes resizeanim { from { opacity: 0; } to { opacity: 0; } } .resize-triggers { animation: 1ms resizeanim; visibility: hidden; opacity: 0; } .resize-triggers, .resize-triggers > div, .contract-trigger:before { content: " "; display: block; position: absolute; top: 0; left: 0; height: 100%; width: 100%; overflow: hidden; } .resize-triggers > div { background: #eee; overflow: auto; } .contract-trigger:before { width: 200%; height: 200%; }</style><style type="text/css">/* Chart.js */
 @keyframes chartjs-render-animation{from{opacity:.99}to{opacity:1}}.chartjs-render-monitor{animation:chartjs-render-animation 1ms}.chartjs-size-monitor,.chartjs-size-monitor-expand,.chartjs-size-monitor-shrink{position:absolute;direction:ltr;left:0;top:0;right:0;bottom:0;overflow:hidden;pointer-events:none;visibility:hidden;z-index:-1}.chartjs-size-monitor-expand>div{position:absolute;width:1000000px;height:1000000px;left:0;top:0}.chartjs-size-monitor-shrink>div{position:absolute;width:200%;height:200%;left:0;top:0}</style></head>
-<body >
+<body class="sidebar-folded">
 	<div class="main-wrapper">
 
 		<!-- partial:partials/_sidebar.html -->
@@ -625,12 +625,15 @@
                 return is_numeric(strpos(strtolower($_SERVER['HTTP_USER_AGENT']), "mobile"));
               }
 
-              echo isMobile() ? "<img src=".base_url()."assets\media\logos/icon.svg'>" : "" ;
-              /* If you are redirecting the user to a mobile page, it is as simple as
-              if (isMobile()) {
-                header("Location: http://mobile.yoursite.com/");
-              } */
+              echo isMobile() ? 
+
+                "<img src=".base_url()."assets\media\logos/icon.svg>" 
+              : "<img width=20 src=".base_url()."assets\media\logos/icon.svg>" ;
+
+
               ?>
+              
+
 
       
 
@@ -802,7 +805,7 @@
 					<?php
 
               if (isMobile()){ ?> 
-                
+
                 &nbsp;&nbsp; <img src="<?php echo base_url() ?>assets\media\logos/logo rouge.svg" style="width: 55px;" >  <?php } 
 
                 ?>
