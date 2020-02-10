1aa809e225a032dd826c0bd494ae8d40661e6aa7
Maiza Bahaedinne <bahaedinne.maiza@esprit.tn>
2020-02-10 23:24:16 +0100

    Update noter.php

--- application/views/project/noter.php
+++ application/views/project/noter.php
@@ -111,7 +111,11 @@
                     </div>
                     <div class="row">
                       <div class="col-12 col-md-12 col-xl-12">
-                        <h4 class="mb-1"><?php echo if($projectInfo->eventFB !='' ) { ?> <a href="<?php echo $projectInfo->createdDate ; ?>" > Aller au facebook </a> <?php }  ?></h4>                       
+                        <h4 class="mb-1">
+                          <?php echo if ( $projectInfo->eventFB !='' ) { ?>
+                           <a href="<?php echo $projectInfo->createdDate ; ?>" > Aller au facebook </a>
+                            <?php }  ?>
+                        </h4>                       
                       </div>
                     </div>
 
