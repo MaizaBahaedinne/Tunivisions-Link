1db3421e207a23f4171796d07a688e116fbbf18c
Maiza Bahaedinne <bahaedinne.maiza@esprit.tn>
2020-02-22 15:24:01 +0100

    register

--- application/views/register/new.php
+++ application/views/register/new.php
@@ -74,7 +74,7 @@
               <hr>
             
 
-            <?php if ($var1!=Null && ($var2  == 2 || $var2 == 3 )  ) { ?> 
+            <?php if ($var1!=Null && ($var2  !=Null )  ) { ?> 
 
             
             <div class="form-group">
