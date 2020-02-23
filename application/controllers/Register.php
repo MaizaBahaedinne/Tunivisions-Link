8392aa8e8215b1843729f73b47d918805edc1a51
Maiza Bahaedinne <bahaedinne.maiza@esprit.tn>
2020-02-23 12:26:41 +0100

    Update Register.php

--- application/controllers/Register.php
+++ application/controllers/Register.php
@@ -192,7 +192,7 @@ class Register extends CI_Controller
                 }
                 else
                 {
-                    $this->session->set_flashdata('error', 'Problème veuillez contacter l\'équipe  <a href="mailto:tunivisions.link@gmail.com"> tunivisions.link@gmail.com </a> ');
+                    $this->session->set_flashdata('error', 'Problème veuillez contacter l\'équipe support  <a href="mailto:tunivisions.link@gmail.com"> tunivisions.link@gmail.com </a> ');
                 }
 
                     
