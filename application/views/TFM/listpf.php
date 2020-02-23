f7c8f3a82de570d305cd74041bac12f0a283c002
Maiza Bahaedinne <bahaedinne.maiza@esprit.tn>
2020-02-23 16:09:53 +0100

    Update listpf.php

--- application/views/TFM/listpf.php
+++ application/views/TFM/listpf.php
@@ -4,7 +4,7 @@
             <div class="card-header">
         <h4><?php echo count($userRecords) ?> Participant</h4>
         <?php if($clubID == 0 ){ ?>
-            <a href="<?php echo base_url() ?>TFM/partantF">statistiques</a>
+            <a href="<?php echo base_url() ?>TFM/stats">statistiques</a>
         <?php } ?>
     </div>
         <div class="card-body">
