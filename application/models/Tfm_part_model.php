34e1477d2659a6802589beacd14a1d2ebbec437c
Maiza Bahaedinne <bahaedinne.maiza@esprit.tn>
2020-02-26 23:15:20 +0100

    Update Tfm_part_model.php

--- application/models/Tfm_part_model.php
+++ application/models/Tfm_part_model.php
@@ -117,7 +117,7 @@ class Tfm_part_model extends CI_Model
      * @param number $segment : This is pagination limit
      * @return array $result : This is result
      */
-    function TFMPartListinByclub($clubId)
+    function TFMPartListinByclubV($clubId)
     {
         $this->db->select(' BaseTbl.id , Users.name , Clubs.name ClubName , Role.role , Users.cellule , Users.gouvernorat , BaseTbl.p_tranch1 , BaseTbl.p_tranch2 , BaseTbl.moto , BaseTbl.sysMobile , BaseTbl.statut , Usersr1.name as recp1 , BaseTbl.dateP_tranch1 as dateTranche1 , Usersr2.name as recp2 , BaseTbl.dateP_tranch2 as dateTranche2 ');
         $this->db->from('tbl_tfm_part as BaseTbl');
