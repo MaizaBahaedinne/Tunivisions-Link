d706e6850e2ca859a918d0f133e221d865270682
Maiza Bahaedinne <bahaedinne.maiza@esprit.tn>
2020-02-29 15:16:40 +0100

    Update PaimentByClub.php

--- application/views/TFM/PaimentByClub.php
+++ application/views/TFM/PaimentByClub.php
@@ -18,12 +18,7 @@
 				<?php } ?>
 
 
-				<div class="form-check form-check-flat form-check-primary">
-										<label class="form-check-label">
-											<input type="checkbox" class="form-check-input">
-											Remember me
-										</label>
-									</div>
+
 				
 			</div>
 			<div class="card-footer">
