2219b016e3280e0a2280668a8827be94e52d3202
Maiza Bahaedinne <bahaedinne.maiza@esprit.tn>
2020-02-29 15:49:09 +0100

    Update PaimentByClub.php

--- application/views/TFM/PaimentByClub.php
+++ application/views/TFM/PaimentByClub.php
@@ -5,7 +5,7 @@
 		<div class="card">
 			<form action="<?php echo base_url() ?>TFM/partanTfmPaiement1" method="post">
 			<div class="card-header">
-				<h5>Liste des membre participant au TFM </h5>		
+				<h5>Liste des membre participant au TFM | <?php echo count($userRecords) ?> </h5>		
 			</div>
 			<div class="card-body">
 				
@@ -33,7 +33,7 @@
 		<div class="card">
 			
 			<div class="card-header">
-				<h5>Liste des membre validé TRANCHE 1 </h5>				
+				<h5>Liste des membre validé TRANCHE 1 | <?php echo count($userRecordsT1) ?> </h5>				
 			</div>
 			<div class="card-body">
 				<?php foreach ($userRecordsT1 as $record ) { ?>
@@ -56,15 +56,11 @@
 		<div class="card">
 			
 			<div class="card-header">
-				<h5>Liste des membre validé TRANCHE 2 </h5>				
+				<h5>Liste des membre validé TRANCHE 2  | <?php echo count($userRecordsT2) ?> </h5>				
 			</div>
 			<div class="card-body">
 				<?php foreach ($userRecordsT2 as $record ) { ?>
-					<div class="form-check form-check-flat form-check-primary">
-						<label class="form-check-label">
-							<input class="form-check-input" type="checkbox" name="participant[]" value="<?php echo $record->id ; ?>" > <?php echo ' '.$record->name ;  ?>
-						</label>
-					</div>
+					 <?php echo ' '.$record->name ;  ?>
 				<?php } ?>
 			</div>
 			<div class="card-footer">
