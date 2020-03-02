55c6b5bb91699e8a5ebf32487ecc979f5fa738fe
Maiza Bahaedinne <bahaedinne.maiza@esprit.tn>
2020-03-02 21:13:37 +0100

    Update stat.php

--- application/views/TFM/stat.php
+++ application/views/TFM/stat.php
@@ -1,5 +1,5 @@
 <div class="row">
-          <div class="col-lg-5 col-xl-4 grid-margin grid-margin-xl-0 stretch-card">
+          <div class="col-lg-5 col-xl-6 grid-margin grid-margin-xl-0 stretch-card">
             <div class="card">
               <div class="card-body">
                 <div class="d-flex justify-content-between align-items-baseline mb-2">
@@ -41,13 +41,22 @@
               </div>
             </div>
           </div>
-          <div class="col-lg-7 col-xl-8 stretch-card">
+          <div class="col-lg-7 col-xl-6 stretch-card">
             <div class="card">
               <div class="card-body">
                 <div class="d-flex justify-content-between align-items-baseline mb-2">
                   <h6 class="card-title mb-0">Classement des clubs </h6>
                 </div>
                 <div class="table-responsive" style="height:600px ;overflow:auto;">
+
+
+                   
+                     
+                          <h6 class="card-title">Donut chart</h6>
+                          <div id="apexDonut"></div>
+                     
+    
+
 
                 </div>
               </div> 
@@ -64,4 +73,40 @@
       $(document).ready( function () {
     $('table').DataTable();
 } );
+  </script>
+
+
+  <script defer src="<?php echo base_url() ?>assets/vendors/apexcharts/apexcharts.min.js"></script>
+
+
+  <script type="text/javascript">
+ 
+
+  $(function() {
+  'use strict';
+    var chart = new ApexCharts(document.querySelector("#apexDonut"), options);
+        
+        chart.render();
+        // Apex Donut chart start
+        
+        // Apex Pie chart end
+        var options = {
+          chart: {
+            height: 300,
+            type: "pie"
+          },
+          colors: ["#f77eb9", "#7ee5e5","#4d8af0","#fbbc06"],
+          legend: {
+            position: 'top',
+            horizontalAlign: 'center'
+          },
+          stroke: {
+            colors: ['rgba(0,0,0,0)']
+          },
+          dataLabels: {
+            enabled: false
+          },
+          series: [44, 55, 13, 33]
+        };
+        });
   </script>
