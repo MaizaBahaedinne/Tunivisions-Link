2f0022b6eeb60c25a75bd1e37ab5d6be53956d55
Maiza Bahaedinne <bahaedinne.maiza@esprit.tn>
2020-03-02 21:15:29 +0100

    Update stat.php

--- application/views/TFM/stat.php
+++ application/views/TFM/stat.php
@@ -79,34 +79,4 @@
   <script defer src="<?php echo base_url() ?>assets/vendors/apexcharts/apexcharts.min.js"></script>
 
 
-  <script type="text/javascript">
- 
-
-  $(function() {
-  'use strict';
-    var chart = new ApexCharts(document.querySelector("#apexDonut"), options);
-        
-        chart.render();
-        // Apex Donut chart start
-        
-        // Apex Pie chart end
-        var options = {
-          chart: {
-            height: 300,
-            type: "pie"
-          },
-          colors: ["#f77eb9", "#7ee5e5","#4d8af0","#fbbc06"],
-          legend: {
-            position: 'top',
-            horizontalAlign: 'center'
-          },
-          stroke: {
-            colors: ['rgba(0,0,0,0)']
-          },
-          dataLabels: {
-            enabled: false
-          },
-          series: [44, 55, 13, 33]
-        };
-        });
-  </script>
+  <script src="../../../assets/js/apexcharts.js"></script>
